7886a4773fe5738db2052d0fd7ef7a31f4152be0
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2023-01-08 15:31:20 +0300

    Update quiz_ui.py

---
+++ quiz_ui.py
@@ -0,0 +1,84 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Aws quiz App")
+        self.window.geometry("850x530")
+
+        # Display Title
+        self.display_title()
+
+        # Creating a canvas for question text, and dsiplay question
+        self.canvas = Canvas(width=800, height=250)
+        self.question_text = self.canvas.create_text(400, 125,
+                                                     text="Question here",
+                                                     width=680,
+                                                     fill=THEME_COLOR,
+                                                     font=(
+                                                         'Ariel', 15, 'italic')
+                                                     )
+        self.canvas.grid(row=2, column=0, columnspan=2, pady=50)
+        self.display_question()
+
+        # Declare a StringVar to store user's answer
+        self.user_answer = StringVar()
+
+        # Display four options(radio buttons)
+        self.opts = self.radio_buttons()
+        self.display_options()
+
+        # To show whether the answer is correct or wrong
+        self.feedback = Label(self.window, pady=10, font=("ariel", 15, "bold"))
+        self.feedback.place(x=300, y=380)
+
+        # Next and Quit Button
+        self.buttons()
+
+        # Mainloop
+        self.window.mainloop()
+
+    def display_title(self):
+        """To display title"""
+
+        title = Label(self.window, text="iQuiz Application",
+                        width=50, bg="green", fg="white", font=("ariel", 20, "bold"))
+        title.place(x=0, y=2)
+
+    def display_question(self):
+        """To display the question"""
+
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def radio_buttons(self):
+        """To create four options (radio buttons)"""
+        # initialize the list with an empty list of options
+        choice_list = []
+
+        # position of the first option
+        y_pos = 220
+
+        # adding the options to the list
+        while len(choice_list) < 4:
+
+            # setting the radio button properties
+            radio_btn = Radiobutton(self.window, text="", variable=self.user_answer, value='', font=("ariel", 14))
+
+            # adding the button to the list
+            choice_list.append(radio_btn)
+
+            # placing the button
+            radio_btn.place(x=200, y=y_pos)
+
+            # incrementing the y-axis position by 40
+            y_pos += 40
+
+        # return the radio buttons
+        return choice_list
+
+    
