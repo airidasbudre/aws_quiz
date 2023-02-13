67ec76a01d1676851884bf2c81d624c0ed9652b1
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2023-02-13 09:25:46 +0300

    10 more questions added

--- quiz_data.py
+++ quiz_data.py
@@ -246,19 +246,7 @@ question_data = [
           'AWS CloudFront'
           ]
       },
-      # 20
-    {
-      'category': 'AWS', 
-      'type': 'multiple', 
-      'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
-      'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
-          ]
-      },
+      
     {
       'category': 'AWS', 
       'type': 'multiple', 
@@ -276,108 +264,263 @@ question_data = [
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'For which of the following AWS resources, the Customer is responsible for the infrastructure-related security configurations?',
+      'correct_answer': 'Amazon EC2', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'Amazon RDS', 
+          'Amazon DynamoDB', 
+          'AWS Fargate'
           ]
       },
+      # 22
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'In the shared responsibility model for infrastructure services, such as Amazon Elastic Compute Cloud, which of the below are customers responsibility?',
+      'correct_answer': 'Amazon Machine Images (AMIs)', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'Network infrastructure', 
+          'Virtualization infrastructure', 
+          'Physical security of hardware'
           ]
       },
+      # 23
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'AWS offers two savings plans to enable more savings and flexibility for its customers, namely, compute saving plans and EC2 Instance Savings plans. Which of the below statement is FALSE regarding Saving Plans?',
+      'correct_answer': 'Savings Plans are available for all the regions.', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'Capacity Reservations are not provided with Saving Plans.', 
+          'Savings plans will apply on ‘On-Demand Capacity Reservations’ that customers can allocate for their needs.', 
+          'The prices for Savings Plans do not change based on the amount of hourly commitment.'
           ]
       },
+      # 24
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'Which of the below-listed services is a region-based AWS service?',
+      'correct_answer': 'Amazon EFS', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'AWS IAM', 
+          'Amazon Route 53', 
+          'Amazon CloudFront'
           ]
       },
+      # 25
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'Which of the following LightSail Wizard allows the customers to “create a copy of the LightSail instance in EC2”?',
+      'correct_answer': 'Upgrade to EC2', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'LightSail Backup', 
+          'LightSail Copy', 
+          'LightSail-EC2 snapshot'
           ]
       },
+      # 26
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'Which of the following features of Amazon Connect helps better customer engagement on AWS Cloud ?',
+      'correct_answer': 'High Quality Audio', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'Push Notification', 
+          'Mailbox Simulator', 
+          'Reputation Dashboard'
           ]
       },
+      # 27
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'A large IT company is looking to enable its large user base to remotely access Linux desktops from any location. Which service can be used for this purpose ?',
+      'correct_answer': 'Amazon WorkSpaces', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'Amazon Cognito', 
+          'Amazon AppStream 2.0', 
+          'Amazon WorkLink'
           ]
       },
+      # 28
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'Users in the Developer Team need to deploy a multi-tier web application. Which service can be used to create a customized portfolio that will help users for quick deployment?',
+      'correct_answer': 'AWS Service Catalog', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
-          'AWS Trusted Advisor'
+          'AWS Config', 
+          'AWS Code Deploy', 
+          'AWS Cloud Formation'
           ]
       },
+      # 29
       {
       'category': 'AWS', 
       'type': 'multiple', 
       'difficulty': 'medium', 
-      'question': 'Which tool can you use to forecast your AWS spending?',
-      'correct_answer': 'AWS Cost Explorer', 
+      'question': 'A large Oil & gas company is planning to deploy a high-volume application on multiple Amazon EC2 instances.  Which of the following can help to reduce operational expenses?',
+      'correct_answer': 'Deploy Amazon EC2 instance with Auto-scaling', 
       'incorrect_answers': [
-          'AWS Organizations', 
-          'Amazon Dev Pay', 
+          'Deploy Amazon EC2 instance in multiple AZ’s', 
+          'Deploy Amazon EC2 instance with Amazon instance store-backed AMI', 
+          'Deploy Amazon EC2 instance with Cluster placement group'
+          ]
+      },
+      # 30
+    {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'Which of the following activity are within the scope of AWS Support?',
+      'correct_answer': 'Troubleshooting API issues', 
+      'incorrect_answers': [
+          'Code Development', 
+          'Debugging custom software', 
+          'Database query tuning'
+          ]
+      },
+      # 31
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'A company is planning to run a global marketing application in the AWS Cloud. The application will feature videos that can be viewed by users. The company must ensure that all users can view these videos with low latency.',
+      'correct_answer': ' Amazon CloudFront ', 
+      'incorrect_answers': [
+          'AWS Auto Scaling', 
+          'Amazon Kinesis Video Streams', 
+          'Elastic Load Balancing'
+          ]
+      },
+      # 32
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'Which pillar of the AWS Well-Architected Framework refers to the ability of a system to recover from infrastructure or service disruptions and dynamically acquire computing resources to meet demand?',
+      'correct_answer': 'Reliability ', 
+      'incorrect_answers': [
+          'Security', 
+          'Performance efficiency', 
+          'Cost optimization'
+          ]
+      },
+      # 33
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'Which of the following are benefits of migrating to the AWS Cloud?',
+      'correct_answer': 'Operational resilience ', 
+      'correct_answer': 'Business agility  ', 
+      'incorrect_answers': [
+          'Discounts for products on Amazon.com', 
+          'Business excellence', 
+          'Increased staff retention'
+          ]
+      },
+      # 34
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'A company is planning to replace its physical on-premises compute servers with AWS serverless compute services. The company wants to be able to take advantage of advanced technologies quickly after the migration.Which pillar of the AWS Well-Architected Framework does this plan represent?',
+      'correct_answer': 'Performance efficiency', 
+      'incorrect_answers': [
+          'Security', 
+          'Operational excellence', 
+          'Reliability'
+          ]
+      },
+      # 35
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'A large company has multiple departments. Each department has its own AWS account. Each department has purchased Amazon EC2 Reserved Instances.Some departments do not use all the Reserved Instances that they purchased, and other departments need more Reserved Instances than they purchased.The company needs to manage the AWS accounts for all the departments so that the departments can share the Reserved Instances.Which AWS service or tool should the company use to meet these requirements?',
+      'correct_answer': 'AWS Organizations ', 
+      'incorrect_answers': [
+          'AWS Systems Manager', 
+          'Cost Explorer', 
           'AWS Trusted Advisor'
+          ]
+      },
+      # 36
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'Which component of the AWS global infrastructure is made up of one or more discrete data centers that have redundant power, networking, and connectivity?',
+      'correct_answer': 'Availability Zone', 
+      'incorrect_answers': [
+          'AWS Region', 
+          'Edge location', 
+          'AWS Outposts'
+          ]
+      },
+      # 37
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'Which duties are the responsibility of a company that is using AWS Lambda? (Choose two.)',
+      'correct_answer': 'Security inside of code ', 
+      'correct_answer': 'Writing and updating of code', 
+      'incorrect_answers': [
+          'Selection of CPU resources', 
+          'Patching of operating system', 
+          'Security of underlying infrastructure'
+          ]
+      },
+      # 38
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'Which AWS services or features provide disaster recovery solutions for Amazon EC2 instances? (Choose two.)',
+      'correct_answer': 'EC2 Amazon Machine Images (AMIs)', 
+      'correct_answer': 'Amazon Elastic Block Store (Amazon EBS) snapshots', 
+      'incorrect_answers': [
+          '¡2 Reserved Instances', 
+          'AWS Shield', 
+          'Amazon GuardDuty'
+          ]
+      },
+      # 39
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'A company is migrating to the AWS Cloud instead of running its infrastructure on premises.Which of the following are advantages of this migration? (Choose two.)',
+      'correct_answer': 'Increased global reach and agility', 
+      'correct_answer': 'Ability to deploy globally in minutes', 
+      'incorrect_answers': [
+          'Elimination of the cost of IT staff members', 
+          'Redundancy by default for all compute services', 
+          'Elimination of the need to perform security auditing'
+          ]
+      },
+      # 40
+      {
+      'category': 'AWS', 
+      'type': 'multiple', 
+      'difficulty': 'medium', 
+      'question': 'A user is comparing purchase options for an application that runs on Amazon EC2 and Amazon RDS. The application cannot sustain any interruption. The application experiences a predictable amount of usage, including some seasonal spikes that last only a few weeks at a time. It is not possible to modify the application.Which purchase option meets these requirements MOST cost-effectively?',
+      'correct_answer': 'Buy Reserved Instances for the predicted amount of usage throughout the year. Allow any seasonal usage to run at an On-Demand rate.', 
+      'incorrect_answers': [
+          'Review the AWS Marketplace and buy Partial Upfront Reserved Instances to cover the predicted and seasonal load.', 
+          'Buy Reserved Instances for the predicted amount of usage throughout the year. Allow any seasonal usage to run on Spot Instances.', 
+          'Buy Reserved Instances to cover all potential usage that results from the seasonal usage.'
           ]
       },
 ]
